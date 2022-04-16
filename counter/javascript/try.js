e6ca3dd82ef0a007e0015768ab3e9abb34822126
heamanbelay <heamanbelay@gmail.com>
2022-04-16 16:58:15 +0300

    Add files via upload

---
+++ counter/javascript/try.js
@@ -0,0 +1,15 @@
+
+//let bonusPoints=50
+
+//bonusPoints=bonusPoints+100
+
+//bonusPoints=bonusPoints-25
+
+//bonusPoints=bonusPoints+70
+
+//console.log(bonusPoints)
+
+function increment(){
+    console.log("this is yyyyyyyy")
+}
+increment()
